31cdd0b6769d5ef23f445335953cde3ff32c5a82
kycool <kycool@yeah.net>
2013-05-18 13:51:25 +0800

    Update ModbusMasterUdp.php

--- Phpmodbus/ModbusMasterUdp.php
+++ Phpmodbus/ModbusMasterUdp.php
@@ -20,28 +20,29 @@ require_once dirname(__FILE__) . '/ModbusMaster.php';
 /**
  * ModbusMasterUdp
  *
- * This class deals with the MODBUS master using UDP stack.
+ * 这个类使用UDP协议处理基于modbus协议的通信
  *  
- * Implemented MODBUS master functions:
- *   - FC  1: read coils
- *   - FC  3: read multiple registers
- *   - FC 15: write multiple coils 
- *   - FC 16: write multiple registers
- *   - FC 23: read write registers
+ * 继承抽象函数列表:
+ *   - FC  1: 读线圈
+ *   - FC  3: 读多个寄存器
+ *   - FC 15: 写多个线圈
+ *   - FC 16: 写多个寄存器
+ *   - FC 23: 读写寄存器
  *   
  * @author Jan Krakora
+ * @internal kycool update
  * @copyright  Copyright (c) 2004, 2012 Jan Krakora
  * @package Phpmodbus  
- *
+ * @version 版本号2.0
  */
 class ModbusMasterUdp extends ModbusMaster {
   
   /**
    * ModbusMasterUdp
    *
-   * This is the constructor that defines {@link $host} IP address of the object. 
+   * 构造函数. 
    *     
-   * @param String $host An IP address of a Modbus UDP device. E.g. "192.168.1.1".
+   * @param String $host  Modbus UDP 设备的IP地址. 例如g. "192.168.1.1".
    */         
   function ModbusMasterUdp($host){
     $this->host = $host;
