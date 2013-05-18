d1d0990b06ba839acff7f44249e65feec9d7d215
kycool <kycool@yeah.net>
2013-05-18 14:07:14 +0800

    Update PhpType.php

--- Phpmodbus/PhpType.php
+++ Phpmodbus/PhpType.php
@@ -1,27 +1,18 @@
 <?php
 /**
- * Phpmodbus Copyright (c) 2004, 2012 Jan Krakora
  *
- * This source file is subject to the "PhpModbus license" that is bundled
- * with this package in the file license.txt.
- *
- * @author Jan Krakora
- * @copyright Copyright (c) 2004, 2012 Jan Krakora
  * @license PhpModbus license
  * @category Phpmodbus
  * @package Phpmodbus
- * @version $id$
+ * @version 版本号更新2.0
  *
  */
 
 /**
  * PhpType
  *
- * The class includes set of methods that convert the received Modbus data
- * (array of bytes) to the PHP data type, i.e. signed int, unsigned int and float.
+ * 一系列把接受的数据转换为PHP类型的数据
  *
- * @author Jan Krakora
- * @copyright  Copyright (c) 2004, 2012 Jan Krakora
  * @package Phpmodbus
  *
  */
@@ -30,8 +21,6 @@ class PhpType {
     /**
      * bytes2float
      *
-     * The function converts array of 4 bytes to float. The return value
-     * depends on order of the input bytes (endianning).
      *
      * @param array $values
      * @param bool $endianness
@@ -40,20 +29,13 @@ class PhpType {
     public static function bytes2float($values, $endianness = 0) {
         $data = array();
         $real = 0;
-
-        // Set the array to correct form
         $data = self::checkData($values);
-        // Combine bytes
         $real = self::combineBytes($data, $endianness);
-        // Convert the real value to float
         return (float) self::real2float($real);
     }
 
     /**
      * bytes2signedInt
-     *
-     * The function converts array of 2 or 4 bytes to signed integer.
-     * The return value depends on order of the input bytes (endianning).
      *
      * @param array $values
      * @param bool $endianness
@@ -62,23 +44,16 @@ class PhpType {
     public static function bytes2signedInt($values, $endianness = 0) {
         $data = array();
         $int = 0;
-        // Set the array to correct form
         $data = self::checkData($values);
-        // Combine bytes
         $int = self::combineBytes($data, $endianness);
-        // In the case of signed 2 byte value convert it to 4 byte one
         if ((count($values) == 2) && ((0x8000 & $int) > 0)) {
             $int = 0xFFFF8000 | $int;
         }
-        // Convert the value
         return (int) self::dword2signedInt($int);
     }
 
     /**
      * bytes2unsignedInt
-     *
-     * The function converts array of 2 or 4 bytes to unsigned integer.
-     * The return value depends on order of the input bytes (endianning).
      *
      * @param array $values
      * @param bool $endianness
@@ -87,28 +62,20 @@ class PhpType {
     public static function bytes2unsignedInt($values, $endianness = 0) {
         $data = array();
         $int = 0;
-        // Set the array to correct form
         $data = self::checkData($values);
-        // Combine bytes
         $int = self::combineBytes($data, $endianness);
-        // Convert the value
         return self::dword2unsignedInt($int);
     }
 
     /**
      * bytes2string
      *
-     * The function converts an values array to the string. The function detects
-     * the end of the string by 0x00 character as defined by string standards.
-     *
      * @param array $values
      * @param bool $endianness
      * @return string
      */
     public static function bytes2string($values, $endianness = 0) {
-        // Prepare string variable
         $str = "";
-        // Parse the received data word array
         for($i=0;$i<count($values);$i+=2) {
             if ($endianness) {
                 if($values[$i] != 0)
@@ -131,35 +98,23 @@ class PhpType {
                     break;
             }
         }
-        // return string
         return $str;
     }
 
     /**
      * real2float
      *
-     * This function converts a value in IEC-1131 REAL single precision form to float.
-     *
-     * For more see [{@link http://en.wikipedia.org/wiki/Single_precision Single precision on Wiki}] or
-     * [{@link http://de.php.net/manual/en/function.base-convert.php PHP base_convert function commentary}, Todd Stokes @ Georgia Tech 21-Nov-2007] or
-     * [{@link http://www.php.net/manual/en/function.pack.php PHP pack/unpack functionality}]
-     *
-     * @param value value in IEC REAL data type to be converted
+     * @param value value in IEC REAL 
      * @return float float value
      */
     private static function real2float($value) {
-        // get unsigned long
         $ulong = pack("L", $value);
-        // set float
         $float = unpack("f", $ulong);
-        
         return $float[1];
     }
 
     /**
      * dword2signedInt
-     *
-     * Switch double word to signed integer
      *
      * @param int $value
      * @return int
@@ -175,8 +130,6 @@ class PhpType {
     /**
      * dword2signedInt
      *
-     * Switch double word to unsigned integer
-     *
      * @param int $value
      * @return int|float
      */
@@ -191,25 +144,20 @@ class PhpType {
     /**
      * checkData
      *
-     * Check if the data variable is array, and check if the values are numeric
-     *
      * @param int $data
      * @return int
      */
     private static function checkData($data) {
-        // Check the data
         if (!is_array($data) ||
                 count($data)<2 ||
                 count($data)>4 ||
                 count($data)==3) {
             throw new Exception('The input data should be an array of 2 or 4 bytes.');
         }
-        // Fill the rest of array by zeroes
         if (count($data) == 2) {
             $data[2] = 0;
             $data[3] = 0;
         }
-        // Check the values to be number
         if (!is_numeric($data[0]) ||
                 !is_numeric($data[1]) ||
                 !is_numeric($data[2]) ||
@@ -222,8 +170,6 @@ class PhpType {
 
     /**
      * combineBytes
-     *
-     * Combine bytes together
      *
      * @param int $data
      * @param bool $endianness
